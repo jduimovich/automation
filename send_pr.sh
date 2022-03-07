bab939946cc1fea683f9c5e0561013b1ed12906b
John Duimovich <jduimovich@gmail.com>
2022-03-07 16:14:15 -0500

    details on changes

--- send_pr.sh
+++ send_pr.sh
@@ -5,11 +5,11 @@ CHANGE=$(date +"%Y-%m-%d-%H%M%S")
 
 # create random branch with the change to 
 git checkout -b auto-$CHANGE 
-echo $CHANGE > random.txt
+echo "This is a change at $CHANGE" > random.txt
 git add random.txt
 git commit -m "auto-update"
 git push origin auto-$CHANGE
 
-gh pr create -t "test" -b "marketplace-change"
+gh pr create -t "test-$CHANGE" -b "marketplace-change-$CHANGE"
 
 git checkout main
